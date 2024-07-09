81a768399c4503246801ab100d8c120b61be632e
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-07-10 03:37:19 +0530

    changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,11 @@
-// require("dotenv").config();
+import dotenv from "dotenv";
+
+dotenv.config();
 /** @type { import("drizzle-kit").Config } */
 export default {
   schema: "./utils/schema.tsx",
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://database-1_owner:8jGCTrWq3ivL@ep-icy-pond-a1ogwapu.ap-southeast-1.aws.neon.tech/database-1?sslmode=require',
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
   }
 };
