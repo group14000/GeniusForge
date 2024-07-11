dbce74b665b23352e0a743d034d769c586b7073d
DIGONTA HALDER <digantahalder14000@gmail.com>
2024-07-11 09:56:15 +0530

    added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
 "use client"
-import Page from "./(auth)/sign-in/[[...sign-in]]/page";
-
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import { LandingPage } from "./_landingPage/LandingPage";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
+  const { isSignedIn } = useUser();
+
   return (
-    <div>
-      <Page/>
+    <div className="min-h-screen flex flex-col">
+      <nav className="flex items-center justify-between p-4 bg-slate-200 text-primary border shadow-md">
+        <div className="flex items-center">
+          <Image src="/logo.svg" alt="logo" width={120} height={100} />
+        </div>
+        <div className="flex items-center space-x-4">
+          {!isSignedIn && <SignInButton />}
+          {isSignedIn && (
+            <>
+              <Link href="/dashboard">
+                <Button className="text-white bg-primary hover:bg-gray-100">Dashboard</Button>
+              </Link>
+              <UserButton />
+            </>
+          )}
+        </div>
+      </nav>
+      <main className="flex-grow">
+        <LandingPage />
+      </main>
     </div>
   );
 }
